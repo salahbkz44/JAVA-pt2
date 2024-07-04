7c42b37ec2d19116d437a22f04c7c79f6d7d238f
SALAH BKZ <117542403+salahbkz44@users.noreply.github.com>
2024-07-04 14:48:25 +0100

    adding initial content

---
+++ tp02/src/tp02/ex02.java
@@ -0,0 +1,13 @@
+package tp02;
+
+public class ex02 {
+
+	public static void main(String[] args) {
+		String s="University of Setif 1 ";
+		System.out.println(s.indexOf('r'));
+		System.out.println(s.contains("Setif"));
+		System.out.println(s.length());
+		System.out.println(s.charAt(8));
+	}
+
+}
---
+++ tp02/src/tp02/mainClass.java
@@ -0,0 +1,11 @@
+package tp02;
+
+public class mainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(256*2);
+
+	}
+
+}
